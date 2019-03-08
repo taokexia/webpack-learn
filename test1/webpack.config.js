e9a36a5b9caa1f6c5cd2c3fa02d091074dfe3d5f
taokexia <694198149@qq.com>
2019-03-08 22:21:12 +0800

    First commit

---
+++ test1/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: 'babel-loader',
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'less-loader'
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
+  // 代码模块路径解析的配置
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: [".wasm", ".mjs", ".js", ".json", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin('[name].css')
+  ]
+}
